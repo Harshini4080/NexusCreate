1e5f08236cf0dd1be8d2b841e20c7a2a5665b8d0
Harshini Ramavath <harshiniramavath22@gmail.com>
2026-02-26 12:12:28 +0530

    Updated aiController logic and improved structure

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -159,6 +159,36 @@ export const removeImageBackground = async (req, res) => {
   }
 };
 
+export const removeImageObject = async (req, res)=>{
+    try {
+        const { userId } = req.auth();
+        const { object } = req.body;
+        const image = req.file;
+        const plan = req.plan;
+
+        if(plan !== 'premium'){
+            return res.json({ success: false, message: "This feature is only available for premium subscriptions"})
+        }
+
+        const {public_id} = await cloudinary.uploader.upload(image.path)
+
+        const imageUrl = cloudinary.url(public_id, {
+            transformation: [{effect: `gen_remove:${object}`}],
+            resource_type: 'image'
+        })
+
+        await sql` INSERT INTO creations (user_id, prompt, content, type) 
+        VALUES (${userId}, ${`Removed ${object} from image`}, ${imageUrl}, 'image')`;
+
+        res.json({ success: true, content: imageUrl})
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+
 /* Resume Review (Premium) */
 export const resumeReview = async (req, res) => {
   try {
